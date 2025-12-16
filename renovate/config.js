b4c2a978fe29bd257e99df82290ff058a77d03a5
Erwan Leboucher <erwanleboucher@gmail.com>
2025-12-16 19:51:49 +0100

    update renovate config

--- renovate/config.js
+++ renovate/config.js
@@ -2,6 +2,4 @@ module.exports = {
       platform: 'github',
       autodiscover: false,
       onboarding: false,
-      requireConfig: 'optional',
-      gitAuthor: process.env.RENOVATE_GIT_AUTHOR,
     };
